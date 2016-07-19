8973f26334cfdbac12ed3069cc625810da55fca1
Cyril Tsiboulski <ct@cloudred.com>
2016-07-19 00:52:18 -0400

    adding facebook meta tags

--- wp-content/themes/wellmet/templates/head.php
+++ wp-content/themes/wellmet/templates/head.php
@@ -3,4 +3,8 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php wp_head(); ?>
+  <meta property="og:title" content="<?php get_bloginfo('name'); ?>"/>
+  <meta property="og:image" content="<?php get_bloginfo('template_directory '); ?>/dist/images/facebook.png"/>
+  <meta property="og:site_name" content="<?php get_bloginfo('name'); ?>"/>
+  <meta property="og:description" content="<?php get_bloginfo('description'); ?>"/>
 </head>
