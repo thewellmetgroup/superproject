b20d08a424a9081da566c166c5337c5141c121c8
Cyril Tsiboulski <3483587+mrlosh@users.noreply.github.com>
2020-11-13 10:56:19 -0500

    Added password check

--- wp-content/themes/wellmet/templates/content-page.php
+++ wp-content/themes/wellmet/templates/content-page.php
@@ -5,20 +5,22 @@
 		<div class="col-md-8">
 
 			<?php
-			if( have_rows('quotes') ): 
-				echo '<div id="quotes" class="quotes">';
-				// Loop through rows.
-			    while( have_rows('quotes') ) : the_row();
-			        // Load sub field value.
-			        $quote = get_sub_field('quote');
-			        echo '<blockquote>';
-						echo $quote;
-						echo '”';
-					echo '</blockquote>';
-			    // End loop.
-			    endwhile;
-			    echo '</div>';
-			endif;
+			if ( ! post_password_required() ) {
+				if( have_rows('quotes') ): 
+					echo '<div id="quotes" class="quotes">';
+					// Loop through rows.
+				    while( have_rows('quotes') ) : the_row();
+				        // Load sub field value.
+				        $quote = get_sub_field('quote');
+				        echo '<blockquote>';
+							echo $quote;
+							echo '”';
+						echo '</blockquote>';
+				    // End loop.
+				    endwhile;
+				    echo '</div>';
+				endif;
+			}
 			?>	
 					
 			<?php the_content(); ?>
@@ -26,9 +28,11 @@
 			<?php 
 
 			//Grantee Resources
-			if( have_rows('resource_section') ):
-				include( locate_template( 'templates/content-resources.php' ) );
-			endif; ?>
+			if ( ! post_password_required() ) {
+				if( have_rows('resource_section') ):
+					include( locate_template( 'templates/content-resources.php' ) );
+				endif;
+			} ?>
 
 			<?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
 		</div>
@@ -37,11 +41,13 @@
 	</div>
 	<?php
 	//Member Impact Areas
+	if ( ! post_password_required() ) {
 		if( have_rows('membership_benefits') ):
 			echo '<div class="row membership-areas">';
 				include( locate_template( 'templates/content-membership.php' ) );
 			echo '</div>';
 		endif;
 		//END
+	}
 	?>
 </div>
